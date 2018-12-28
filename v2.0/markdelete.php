b38b99541dce7842bd0ce84940a06c4f64c3c7f2
Beck <Meta@Rebeccas-MacBook-Pro.local>
2018-12-28 23:00:15 +1030

    Added error handling, just in case an invalid task_id is injected.

--- v2.0/markdelete.php
+++ v2.0/markdelete.php
@@ -2,7 +2,10 @@
       require_once('../../strict-typing.php');
       require_once('tasks.php');
 
-  if(isset($_POST['submit'])){
+  if(isset($_POST['submit']) && isset($tasklist[0]->tasks[$_POST['mark']])){
     $tasklist[0]->tasks[$_POST['mark']]->MarkComplete();
+  }
+  else{
+    echo "Sorry, something went wrong, please try again.";
   }
 ?>
