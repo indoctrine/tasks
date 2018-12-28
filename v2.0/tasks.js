0823e2594e7a103de72e638561ddd9accdaac507
Beck <Meta@Rebeccas-MacBook-Pro.local>
2018-12-28 22:59:03 +1030

    Comments, dynamic updating of completion and colour coding for dates.

--- v2.0/tasks.js
+++ v2.0/tasks.js
@@ -1,4 +1,19 @@
+/*
+                    Task List
+    CHANGES:
+      28 December 2018  - File created.
+                        - Makes AJAX call to markdelete.php to set marked on records.
+                        - Updates the buttons and column values in the table.
+                        - Colours date fields in red where they are due today or overdue.
+*/
+
+
 $(document).ready(() => {
+  today = $.date('Y-m-d');
+  $('.due_col').each(function(){
+    $(this).html() <= today ? $(this).css('color', 'red') : ''; //Jen loves ternaries
+  });
+
   $('.mark').on('click', function() {
     const mark = $(this).val();
 
@@ -9,7 +24,17 @@ $(document).ready(() => {
           'mark': mark
         },
         response => {
+          if(response == 1){
+            $(this).html('✕');
+            $('#row_' + mark).children('.comp_col').html('Yes');
+          }
+          else if(response == 0){
+            $(this).html('✓');
+            $('#row_' + mark).children('.comp_col').html('No');
+          }
+          else{
             $('#output').html(response);
+          }
         }
     );
   });
